2701f770d46040f1634d215136f5d50bf475612a
pophils <kpophils@gmail.com>
2015-07-24 15:10:03 +0100

    Add new functional test

--- yasanaproject/tests/functional/pages/landing.py
+++ yasanaproject/tests/functional/pages/landing.py
@@ -14,3 +14,10 @@ class LandingPage(BasePage):
             self.browser.find_element_by_id('landing-welcome')
         except:
             raise PageException('Landing page not loaded')
+
+    def task_summary_p_tag_count(self):
+        p_summary_count = self.browser.find_elements_by_class_name('summary-count')
+        return len(p_summary_count)
+
+    def get_nav_link_first_item_text(self):
+        return self.browser.find_element_by_css_selector('li.active').text
--- yasanaproject/tests/functional/test_landing.py
+++ yasanaproject/tests/functional/test_landing.py
@@ -1,11 +1,11 @@
 
-from django.test import LiveServerTestCase
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from .pages.landing import LandingPage
 from selenium import webdriver
 from unittest import skip
 
 
-class LandingViewTestCase(LiveServerTestCase):
+class LandingViewTestCase(StaticLiveServerTestCase):
 
     def setUp(self):
         self.browser = webdriver.Firefox()
@@ -33,3 +33,30 @@ class LandingViewTestCase(LiveServerTestCase):
         self.page.first_visit(self.live_server_url)
 
         self.assertEqual(self.browser.title.lower(), 'yasana --- enterprise social network.')
+
+    def test_logged_in_user_can_see_task_summary(self):
+
+        self.page = LandingPage(self.browser)
+
+        self.page.pre_login(self.live_server_url)
+
+        self.page.first_visit(self.live_server_url)
+
+        p_summary_count = self.page.task_summary_p_tag_count()
+
+        self.assertTrue(p_summary_count >= 3)
+
+    def test_logged_in_user_can_see_necessary_nav_links(self):
+
+        self.page = LandingPage(self.browser)
+
+        self.page.pre_login(self.live_server_url)
+
+        self.page.first_visit(self.live_server_url)
+
+        li_item_text = self.page.get_nav_link_first_item_text()
+
+        self.assertIn('dashboard', li_item_text.lower())
+
+
+
